0a0879bda205c44f8d79e973d57c8dae6adb244a
Fabiano Alcantara <105230664+FabianoAlcantarabr@users.noreply.github.com>
2022-09-29 19:54:36 -0300

    Add files via upload

---
+++ src/main/java/ApiIfoodDevWeek/Repository/ClienteRepository.java
@@ -0,0 +1,10 @@
+package ApiIfoodDevWeek.Repository;
+
+
+import ApiIfoodDevWeek.Model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+}
---
+++ src/main/java/ApiIfoodDevWeek/Repository/RestauranteRepository.java
@@ -0,0 +1,10 @@
+package ApiIfoodDevWeek.Repository;
+
+
+import ApiIfoodDevWeek.Model.Restaurante;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
+}
