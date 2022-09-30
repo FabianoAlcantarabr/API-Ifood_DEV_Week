da03e9e22b7f6e3b535b299d0d9e18c6f5a8a847
Fabiano Alcantara <105230664+FabianoAlcantarabr@users.noreply.github.com>
2022-09-29 22:03:07 -0300

    Delete API-Ifood_DEV_Week directory

--- API-Ifood_DEV_Week/src/main/java/ApiIfoodDevWeek/Repository/ItemRepository.java
+++
@@ -1,9 +0,0 @@
-package ApiIfoodDevWeek.Repository;
-
-import ApiIfoodDevWeek.Model.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ItemRepository extends JpaRepository<Item, Long> {
-}
